c06a65100280155ec896d202ea63907398ac9e30
mateusz <mateusz.hladky@gmail.com>
2021-11-05 21:59:56 +0100

    Finished home page for mobile. Finished animations. Added horizontal scrolling to the section. Added footer. Support currently only for mobile.

--- js/index.js
+++ js/index.js
@@ -17,14 +17,18 @@ const heroScroll = () => {
 
 heroScroll();
 
-// Gsap animations
+// GSAP ANIMATIONS
+
+// 1. section animations
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(CSSRulePlugin);
 
 const headings = gsap.utils.toArray('.design__heading');
 const articles = gsap.utils.toArray('.design__article');
 const img1 = gsap.utils.toArray('.design__img--p');
 const img2 = gsap.utils.toArray('.design__img--o');
-let tl = gsap.timeline();
+const tl = gsap.timeline();
+const tl2 = gsap.timeline();
 
 headings.forEach((text, i) => {
     gsap.from(text, {
@@ -77,18 +81,57 @@ img2.forEach((image, i) => {
     });
 });
 
-gsap.timeline({
-    scrollTrigger: {
-        trigger: '.horizontal',
-        start: 'center center',
-        end: 'bottom top',
-        markers: false,
-        scrub: 1,
-        pin: true,
-    },
-})
-    .from('.horizontal__heading', { y: 150, opacity: 0 })
-    .from('#hor-art-1', { x: innerWidth * 1 })
-    .from('#hor-art-2', { x: innerWidth * -1 }, '+=0.5')
-    .to('#hor-art-1', { x: -400, opacity: 0 })
-    .to('#hor-art-2', { x: 500, opacity: 0 }, '+=0.5');
+// 2. horizontal scrolling
+
+tl.from('.horizontal__sec1 h1', { y: 200, opacity: 0, duration: 4 })
+    .from('.horizontal__article', {
+        xPercent: -100,
+        opacity: 0,
+        duration: 5,
+        stagger: 4,
+    })
+    .to('.horizontal', { x: -window.innerWidth, duration: 5 })
+    .from('.horizontal__sec2 h1', { y: 200, opacity: 0, duration: 4 })
+    .from('.horizontal__img', {
+        xPercent: 100,
+        opacity: 0,
+        duration: 5,
+        stagger: 3,
+    })
+    .to('.horizontal', { x: -window.innerWidth * 2, duration: 5 })
+    .from('.horizontal__sec3 h2', { opacity: 0, scale: 3, duration: 5 });
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger: '.horizontal',
+    start: 'center center',
+    end: '+=4000',
+    scrub: true,
+    pin: true,
+});
+
+// 3. on page load
+const subBefore = CSSRulePlugin.getRule('.hero__subtitle-underline::before');
+const btnBefore = CSSRulePlugin.getRule('.hero__button::before');
+
+tl2.fromTo(
+    '.hero__title',
+    { x: -600, opacity: 0 },
+    { x: 0, opacity: 1, duration: 1.5, ease: Power4.easeOut }
+)
+    .from('.hero__subtitle', {
+        xPercent: 100,
+        opacity: 0,
+        ease: Power4.easeOut,
+    })
+    .fromTo(
+        subBefore,
+        { width: 0 },
+        { width: '100%', duration: 0.75, ease: Power4.easeOut }
+    )
+    .from('.hero__button-wrapper', { y: 50, opacity: 0, duration: 1 })
+    .fromTo(
+        btnBefore,
+        { width: 0 },
+        { width: '70px', duration: 0.25, ease: Power3.easeOut }
+    );
